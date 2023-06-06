84ebddd3869f146871fb30176e53fd065ab72550
Oksana Karpova <oksanachv@gmail.com>
2023-06-06 12:17:27 +0200

    Dot vs Bracket notation assignment to be done

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -92,3 +92,79 @@ const percentages = [
   percentageOfWorld1(populations[3]),
 ];
 console.log(percentages);
+
+// LECTURE: Basic Array Operations (Methods)
+// 1. Create an array containing all the neighbouring countries of a country of your
+// choice. Choose a country which has at least 2 or 3 neighbours. Store the array
+// into a variable called 'neighbours'
+// 2. At some point, a new country called 'Utopia' is created in the neighbourhood of
+// your selected country. So add it to the end of the 'neighbours' array
+// 3. Unfortunately, after some time, the new country is dissolved. So remove it from
+// the end of the array
+// 4. If the 'neighbours' array does not include the country ‘Germany’, log to the
+// console: 'Probably not a central European country :D'
+// 5. Change the name of one of your neighbouring countries. To do that, find the
+// index of the country in the 'neighbours' array, and then use that index to
+// change the array at that index position. For example, you can search for
+// 'Sweden' in the array, and then replace it with 'Republic of Sweden'.
+
+const neighbours = [
+  "Poland",
+  "Moldova",
+  "Slovakia",
+  "Hungary",
+  "Romania",
+  "belarus",
+  "russia",
+];
+neighbours.push("Utopia");
+neighbours.pop();
+if (!neighbours.includes("Germany")) {
+  console.log("Probably not a central European country");
+}
+
+const index = neighbours.indexOf("russia");
+neighbours[index] = "error, not found";
+console.log(neighbours);
+
+// LECTURE: Introduction to Objects
+// 1. Create an object called 'myCountry' for a country of your choice, containing
+// properties 'country', 'capital', 'language', 'population' and
+// 'neighbours' (an array like we used in previous assignments)
+
+const country = {
+  country: "Ukraine",
+  capital: "Kyiv",
+  language: "Ukrainian",
+  population: 42,
+  neighbours: [
+    "Poland",
+    "Moldova",
+    "Slovakia",
+    "Hungary",
+    "Romania",
+    "belarus",
+    "error, not found",
+  ],
+};
+
+console.log(country);
+
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  age: 2037 - 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+};
+
+console.log(
+  `${jonas.firstName} has ${jonas.friends.length} friends and his best friend is ${jonas.friends[0]}`
+);
+
+// LECTURE: Dot vs. Bracket Notation
+// 1. Using the object from the previous assignment, log a string like this to the
+// console: 'Finland has 6 million finnish-speaking people, 3 neighbouring countries
+// and a capital called Helsinki.'
+// 2. Increase the country's population by two million using dot notation, and then
+// decrease it by two million using brackets notation
