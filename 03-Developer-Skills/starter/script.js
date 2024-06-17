bc006e9d5e24ede586bf4003c15eb00565bff396
karpovska <oksanachv@gmail.com>
2024-06-17 14:36:42 +0200

    Added configuration to project files

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,12 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = '222';
+
+const calcAge = birthYear => 2037 - birthYear;
+
+console.log(x);
+console.log(calcAge(2000));
+
+// TODO
+// FIXME
