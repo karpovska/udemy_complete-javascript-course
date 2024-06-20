8dc0c082aa15231a30c45326fa97027dab6589f9
karpovska <oksanachv@gmail.com>
2024-06-20 12:08:23 +0200

    Finished Developer Skills section

---
+++ 03-Developer-Skills/starter/coding_challanges.js
@@ -0,0 +1,23 @@
+// Coding Challenge #1
+// Given an array of forecasted maximum temperatures, the thermometer displays a
+// string with the given temperatures. Example: [17, 21, 23] will print "... 17ºC in 1
+// days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+// Your tasks:
+// 1. Create a function 'printForecast' which takes in an array 'arr' and logs a
+// string like the above to the console. Try it with both test datasets.
+// 2. Use the problem-solving framework: Understand the problem and break it up
+// into sub-problems!
+// Test data:
+// § Data 1: [17, 21, 23]
+// § Data 2: [12, 5, -5, 0, 4]
+
+const printForecast = function (arr) {
+	let msg = '';
+	for (let i = 0; i < arr.length; i++) {
+		let dayCount = i + 1;
+		msg += '... ' + arr[i] + 'C in ' + dayCount + ' days ';
+	}
+	return msg;
+};
+
+console.log(printForecast([17, 21, 23]));
--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -5,8 +5,81 @@ const x = '222';
 
 const calcAge = birthYear => 2037 - birthYear;
 
-console.log(x);
-console.log(calcAge(2000));
+//console.log(calcAge(2013));
 
 // TODO
 // FIXME
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+const calcTempAmplitude = function (temp1, temp2) {
+	let temps = temp1.concat(temp2);
+	let max = temps[0];
+	let min = temps[0];
+	for (let i = 0; i < temps.length; i++) {
+		const curTemp = temps[i];
+		if (typeof curTemp !== 'number') continue;
+		if (curTemp > max) max = curTemp;
+		if (curTemp < min) min = curTemp;
+	}
+	console.log(max, min);
+	return max - min;
+};
+
+const amplitude = calcTempAmplitude([3, 5, 1], [9, 0, 5]);
+console.log(amplitude);
+
+const measureKelvin = function () {
+	const measurement = {
+		type: 'temp',
+		unit: 'cels',
+
+		// FIX the bug
+		value: 10,
+	};
+
+	//B) FIND
+	console.table(measurement);
+
+	console.log(measurement.value);
+	//console.warn(measurement.value);
+	//console.error(measurement.value);
+
+	const kelvin = measurement.value + 273;
+	return kelvin;
+};
+
+// A) IDENTIFY the bug
+console.log(measureKelvin());
+
+// Using a debugger
+const calcTempAmplitudeBug = function (temp1, temp2) {
+	let temps = temp1.concat(temp2);
+
+	let max = 0;
+	let min = 0;
+
+	for (let i = 0; i < temps.length; i++) {
+		const curTemp = temps[i];
+		if (typeof curTemp !== 'number') continue;
+
+		if (curTemp > max) max = curTemp;
+		if (curTemp < min) min = curTemp;
+	}
+	console.log(max, min);
+	return max - min;
+};
+
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 0, 5]);
+// A) IDENTIFY
+console.log(amplitudeBug);
+
+const printForecast = function (arr) {
+	let msg = '';
+
+	for (let i = 0; i < arr.length; i++) {
+		msg += `...  ${arr[i]} C in ${i + 1} days `;
+	}
+	return msg;
+};
+
+console.log(printForecast([17, 21, 23]));
+console.log(printForecast([12, 5, -5, 0, 4]));
